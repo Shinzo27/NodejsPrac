8402e8f92afa46cdf3c72ef31b3d60b8d2a7953b
Shinzo27 <20bmiit031@gmail.com>
2024-03-18 23:04:41 +0530

    practice

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 const myServer = http.createServer((req, res) => {
     const log = `${Date.now()}: ${req.url} : New Request received\n`
+    const myUrl = url.parse(req.url, true);
+    console.log(myUrl);
     fs.appendFile("log.txt", log, (err, data)=>{
-        switch(req.url) {
+        switch(myUrl.pathname) {
             case '/' : res.end("Home page");
             break;
-            case '/about' : res.end("Hello, This is Pratham Patel");
+            case '/about' : 
+                const username = myUrl.query.mynum;
+
+                res.end(`Hello, This is ${username}`);
             break;
             default: res.end("404: Not found")
         }
         console.log("new request received!");
-        res.end("Hello from server");
     });
 })
 
