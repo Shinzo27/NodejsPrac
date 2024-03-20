b06d2cd6af527adadb79dc99962c71a9f0d30dc8
Shinzo27 <20bmiit031@gmail.com>
2024-03-21 00:52:20 +0530

    practice

--- express.js
+++ express.js
@@ -1,2 +1,16 @@
-console.log("Helloo Examsssssssss!")
-console.log("Examsssssssss day-2!")
+const http = require("http")
+const express = require("express")
+
+const app = express()
+
+app.get('/', (req,res)=>{
+    res.send("Hello From home page")
+})
+
+app.get('/about', (req,res)=>{
+    res.send(`Hello ${req.query.name} From about page `)
+})
+
+app.listen(8000, ()=>{
+    console.log("Server Started");
+})
