60e4cad1d9f3cb662e613d2a1974d5572512f9dd
Shinzo27 <20bmiit031@gmail.com>
2024-03-23 01:45:39 +0530

    practice

--- Project-1/index.js
+++ Project-1/index.js
@@ -4,8 +4,49 @@ const app = express()
 const PORT = 8000
 
 //Routes
-app.get("/users", (req, res)=>{
+app.get("/api/users", (req, res)=>{
     return res.json(users);
 })
+
+//html rendering
+app.get("/users", (req, res)=>{
+    // return res.json(users);
+    const html = `
+        <ul>
+            ${users.map(user=> `<li>${user.first_name}</li>`).join("")}
+        </ul>
+    `
+    res.send(html)
+})
+
+//to get user with key
+app.get("/api/users/:id", (req,res)=>{
+    const id = Number(req.params.id)
+    const findUser = users.find((user) => user.id === id )
+    return res.json(findUser)
+})
+
+//
+app.post("/api/users", (req, res)=>{
+    res.json({status: "pending"})
+})
+
+app.patch("/api/users/:id", (req, res)=>{
+    //Todo: edit the user
+    res.json({status: "pending"})
+})
+
+app.delete("/api/users/:id", (req, res)=>{
+    //Todo: delete the user
+    res.json({status: "pending"})
+})
+
+//shortcut
+
+//app.route('api/users/:id').get((req,res)=>{
+//     const id = Number(req.params.id)
+//     const findUser = users.find((user) => user.id === id )
+//     return res.json(findUser)
+// }).put((req,res)=>{}).delete(()=>{})
 
 app.listen(PORT,()=> console.log("Server Started"))
