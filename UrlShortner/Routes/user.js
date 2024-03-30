18a541fd734776af3370af5931179b4b2811dc50
Shinzo27 <20bmiit031@gmail.com>
2024-03-30 23:59:30 +0530

    practice

--- UrlShortner/Routes/user.js
+++ UrlShortner/Routes/user.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const { handleUserSignUp } = require("../Controller/user")
 
-const router = express.router()
+const router = express.Router() 
 
 router.post('/', handleUserSignUp)
 module.exports = router
