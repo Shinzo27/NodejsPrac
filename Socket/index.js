a9ef6ff399d436bcdf9e527f3afbed01bd7cf995
Shinzo27 <20bmiit031@gmail.com>
2024-04-07 19:06:10 +0530

    practice

--- Socket/index.js
+++ Socket/index.js
@@ -1,7 +1,23 @@
 const express = require("express")
 const http = require("http")
+const path = require("path")
+const { Server } = require('socket.io')
+
 
 const app = express()
 const server = http.createServer(app)
+const io = new Server(server)
 
+//socket.io
+io.on('connection',(socket)=>{
+    socket.on('message', message=>{
+        io.emit("user-message", message);
+    })
+})
+
+app.use(express.static(path.resolve('./public')))
+
+app.get('/',(req, res)=>{
+    return res.sendFile('/public/index.html');
+})
 server.listen(8000, ()=> console.log("Server Started"))
