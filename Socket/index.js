e7874d6dddcd4ccd12e27b32501dec77375c9b18
Shinzo27 <20bmiit031@gmail.com>
2024-04-07 13:06:18 +0530

    practice

---
+++ Socket/index.js
@@ -0,0 +1,7 @@
+const express = require("express")
+const http = require("http")
+
+const app = express()
+const server = http.createServer(app)
+
+server.listen(8000, ()=> console.log("Server Started"))
