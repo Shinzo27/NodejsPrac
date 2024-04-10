39624f19f45bc458785496c874c6d95bab078637
Shinzo27 <20bmiit031@gmail.com>
2024-04-10 10:50:05 +0530

    practice

--- Crud/index.js
+++ Crud/index.js
@@ -11,7 +11,15 @@ app.set("view engine", "ejs")
 app.set("views", path.resolve('./Views'))
 
 app.get('/',(req,res)=>{
-    res.render("index")
+    return res.render("index")
+})
+
+app.get('/login',(req,res)=>{
+    return res.render("login");
+})
+
+app.get('/register',(req,res)=>{
+    return res.render("register");
 })
 
 app.listen(PORT, ()=>console.log("Server Started"));
