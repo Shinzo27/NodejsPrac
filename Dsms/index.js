b9d54f103a8f843078c7effbc03fba8abd2c9618
Shinzo27 <20bmiit031@gmail.com>
2024-04-19 22:20:39 +0530

    practice

--- Dsms/index.js
+++ Dsms/index.js
@@ -1,10 +1,13 @@
 const express = require("express")
 const path = require('path')
 const userRoute = require('./Routes/User')
+const User = require('./Model/User')
+const mongoose = require('mongoose')
 
 const app = express()
 const PORT = 8000
 
+mongoose.connect('mongodb://127.0.0.1:27017/Dsms').then(console.log("MongoDB Connected"))
 
 app.set("view engine", "ejs")
 app.set("Views", path.resolve('./Views'))
