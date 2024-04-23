d1b93a294b7ef8c1b014802709857f426c0b20e5
Shinzo27 <20bmiit031@gmail.com>
2024-04-23 13:09:36 +0530

    practice

--- Hms/Backend/app.js
+++ Hms/Backend/app.js
@@ -1,8 +1,43 @@
 const express = require('express')
+const { config } = require('dotenv')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const fileUpload = require('express-fileupload')
+const mongoose = require('mongoose')
+const cloudinary = require('cloudinary')
 
 const app = express()
 const PORT = 8000
 
-app.listen(PORT, ()=>{
+config({path: './Config/.env'})
+
+mongoose.connect(process.env.MONGO_URI).then(console.log("DB Connected"))
+
+app.use(
+    cors({
+        origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
+        methods: ['get','post','put','delete'],
+        credentials: true
+    })
+);
+
+cloudinary.v2.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
+app.use(
+    fileUpload({
+        useTempFiles: true,
+        tempFileDir: "/tmp/",
+    })
+)
+
+app.listen(process.env.PORT, ()=>{
     console.log("Server Started")
 })
