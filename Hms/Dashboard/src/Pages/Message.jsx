97d223d3d68d1133f007647c38910c10a5fdc9f4
Shinzo27 <20bmiit031@gmail.com>
2024-04-29 23:16:05 +0530

    practice

--- Hms/Dashboard/src/Pages/Message.jsx
+++ Hms/Dashboard/src/Pages/Message.jsx
@@ -1,10 +1,59 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { Context } from '../main'
+import axios from 'axios'
+import { Navigate } from 'react-router-dom'
 
 const Message = () => {
+  const [message, setMessage] = useState([])
+  const { isAuthenticated } = useContext(Context)
+  
+  useEffect(()=>{
+    const fetchMessages = async() => {
+      try {
+        const { data } = await axios.get("http://localhost:8000/api/v1/message/getAll",{withCredentials: true})
+        setMessage(data.messages)
+      } catch (error) {
+        console.log("Error while fetching message",error)
+      }
+    }
+    fetchMessages()
+  }, [])
+
+  if(!isAuthenticated) return <Navigate to={"/login"}/>
+  
   return (
-    <div>
-      
+    <section className="page messages">
+    <h1>MESSAGE</h1>
+    <div className="banner">
+      {message && message.length > 0 ? (
+        message.map((element) => {
+          return (
+            <div className="card" key={element._id}>
+              <div className="details">
+                <p>
+                  First Name: <span>{element.firstName}</span>
+                </p>
+                <p>
+                  Last Name: <span>{element.lastName}</span>
+                </p>
+                <p>
+                  Email: <span>{element.email}</span>
+                </p>
+                <p>
+                  Phone: <span>{element.phone}</span>
+                </p>
+                <p>
+                  Message: <span>{element.message}</span>
+                </p>
+              </div>
+            </div>
+          );
+        })
+      ) : (
+        <h1>No Messages!</h1>
+      )}
     </div>
+  </section>
   )
 }
 
