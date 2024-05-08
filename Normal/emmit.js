6aa6ef778dcf0e9a51e2f2664d46d34dbe8a19cb
Shinzo27 <20bmiit031@gmail.com>
2024-05-08 19:11:57 +0530

    practice

--- Normal/emmit.js
+++ Normal/emmit.js
@@ -3,7 +3,9 @@ const EventEmitter = require('node:events')
 const eventEmitter = new EventEmitter()
 
 eventEmitter.on('start', (start, end) => {
-    console.log(`started from ${start} to ${end}`);
-  })
+    for(let i=start; i<=end; i++){
+      console.log(i)
+    }
+})
 
 eventEmitter.emit('start', 0, 100)
