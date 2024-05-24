4a7265020c43b6074f866232a9d07a177fcbf84d
Shinzo27 <20bmiit031@gmail.com>
2024-05-24 13:28:10 +0530

    practice

--- Ecomm/src/Components/Expertise/Expertise.jsx
+++ Ecomm/src/Components/Expertise/Expertise.jsx
@@ -38,27 +38,27 @@ const Expertise = () => {
     pauseOnFocus: true
   };
   return (
-    // <div className='flex justify-center items-center'>
-    //   <Slider {...settings}>
-    //             {
-    //                 heroData.map((data)=>(
-    //                     <div key={data.id}>
-    //                         <div className="grid grid-cols-1 sm:grid-cols-2">
-    //                             <div className="flex flex-col justify-center pt-12 gap-4 sm:pl-3 sm:pt-0 text-center sm:text-left order-2 sm:order-1 relative z-10">
-    //                                 <h1 className="text-5xl sm:text-6xl lg:text-5xl font-bold">{data.title}</h1>
-    //                                 <h1 className="text-5xl uppercase text-white sm:text-[80px] md:text-[100px] xl:text-[150px] font-bold">{data.title2}</h1>
-    //                             </div>
-    //                             <div className="order-1 sm:order-2">
-    //                                 <div>
-    //                                     <img src={data.img} alt="image1" className="w-[400px] sm:w-[450px] h-[400px] sm:h-[450px] sm:scale-105 lg:scale-110 object-contain mx-auto drop-shadow-[-8px_4px_6px_rgba(0,0,0,.4)] relative z-40"/>
-    //                                 </div>
-    //                             </div>
-    //                         </div>
-    //                     </div>
-    //                 ))
-    //             }
-    //         </Slider>
-    // </div>
+    <div className='flex justify-center items-center'>
+      <Slider {...settings}>
+                {
+                    heroData.map((data)=>(
+                        <div key={data.id}>
+                            <div className="grid grid-cols-1 sm:grid-cols-2">
+                                <div className="flex flex-col justify-center pt-12 gap-4 sm:pl-3 sm:pt-0 text-center sm:text-left order-2 sm:order-1 relative z-10">
+                                    <h1 className="text-5xl sm:text-6xl lg:text-5xl font-bold">{data.title}</h1>
+                                    <h1 className="text-5xl uppercase text-white sm:text-[80px] md:text-[100px] xl:text-[150px] font-bold">{data.title2}</h1>
+                                </div>
+                                <div className="order-1 sm:order-2">
+                                    <div>
+                                        <img src={data.img} alt="image1" className="w-[400px] sm:w-[450px] h-[400px] sm:h-[450px] sm:scale-105 lg:scale-110 object-contain mx-auto drop-shadow-[-8px_4px_6px_rgba(0,0,0,.4)] relative z-40"/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                }
+            </Slider>
+    </div>
     <>
     </>
   )
