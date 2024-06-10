eca83f1cbee470cac3c4b3287a4d829d53e81ed8
Shinzo27 <20bmiit031@gmail.com>
2024-06-10 21:37:12 +0530

    practice

--- Ecomm/src/Pages/Cart.jsx
+++ Ecomm/src/Pages/Cart.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Navbar from '../Components/Navbar/Navbar'
 import Footer from '../Components/Footer/Footer'
 import driedFruit4 from '../assets/driedfruit4.jpg'
+import driedFruit2 from '../assets/driedfruit2.jpg'
+import driedFruit1 from '../assets/driedfruit1.jpg'
 import Button from '../Components/Shared/Button'
 
 const Cart = () => {
@@ -33,7 +35,77 @@ const Cart = () => {
                         </div>
                         <div>
                             <div className='text-center flex flex-col justify-center items-center'>
-                                <input type="number" name="" id="" className='rounded-md border-none decoration-transparent'/>
+                                <div>
+                                    <button className='w-7 h-7 mr-3 rounded-md bg-gray-400 text-white'>+</button>
+                                    <input type="number" name="" id="" className='rounded-md border-none decoration-transparent text-center w-24' />
+                                    <button className='w-7 h-7 ml-3 rounded-md bg-gray-400 text-white'>-</button>
+                                </div>
+                                <a className=' pt-1 text-blue-600'>remove</a>
+                            </div>
+                        </div>
+                        <div>
+                            <h1 className='text-xl'>
+                                $35.00
+                            </h1>
+                        </div>
+                    </div>
+                    <div className='flex justify-between pt-8'> 
+                        <div className='flex justify-center items-center gap-5'>
+                            <div className='h-28 w-28'>
+                                <img src={driedFruit1} alt="" className='w-full h-full object-contain rounded-lg' />
+                            </div>
+                            <div>
+                                <div>
+                                    <h1 className='text-lg'>Item Name</h1>
+                                    <p className='text-gray-500 text-sm'>Packing Type</p>
+                                </div>
+                                <div className='pt-5'>
+                                    <p>
+                                        ✅ in stock
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <div className='text-center flex flex-col justify-center items-center'>
+                                <div>
+                                    <button className='w-7 h-7 mr-3 rounded-md bg-gray-400 text-white'>+</button>
+                                    <input type="number" name="" id="" className='rounded-md border-none decoration-transparent text-center w-24' />
+                                    <button className='w-7 h-7 ml-3 rounded-md bg-gray-400 text-white'>-</button>
+                                </div>
+                                <a className=' pt-1 text-blue-600'>remove</a>
+                            </div>
+                        </div>
+                        <div>
+                            <h1 className='text-xl'>
+                                $35.00
+                            </h1>
+                        </div>
+                    </div>
+                    <div className='flex justify-between pt-8'> 
+                        <div className='flex justify-center items-center gap-5'>
+                            <div className='h-28 w-28'>
+                                <img src={driedFruit2} alt="" className='w-full h-full object-contain rounded-lg' />
+                            </div>
+                            <div>
+                                <div>
+                                    <h1 className='text-lg'>Item Name</h1>
+                                    <p className='text-gray-500 text-sm'>Packing Type</p>
+                                </div>
+                                <div className='pt-5'>
+                                    <p>
+                                        ✅ in stock
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <div className='text-center flex flex-col justify-center items-center'>
+                                <div>
+                                    <button className='w-7 h-7 mr-3 rounded-md bg-gray-400 text-white'>+</button>
+                                    <input type="number" name="" id="" className='rounded-md border-none decoration-transparent text-center w-24' />
+                                    <button className='w-7 h-7 ml-3 rounded-md bg-gray-400 text-white'>-</button>
+                                </div>
                                 <a className=' pt-1 text-blue-600'>remove</a>
                             </div>
                         </div>
@@ -62,65 +134,11 @@ const Cart = () => {
                         </div>
                         <div>
                             <div className='text-center flex flex-col justify-center items-center'>
-                                <input type="number" name="" id="" className='rounded-md border-none decoration-transparent'/>
-                                <a className=' pt-1 text-blue-600'>remove</a>
-                            </div>
-                        </div>
-                        <div>
-                            <h1 className='text-xl'>
-                                $35.00
-                            </h1>
-                        </div>
-                    </div>
-                    <div className='flex justify-between pt-8'> 
-                        <div className='flex justify-center items-center gap-5'>
-                            <div className='h-28 w-28'>
-                                <img src={driedFruit4} alt="" className='w-full h-full object-contain rounded-lg' />
-                            </div>
-                            <div>
                                 <div>
-                                    <h1 className='text-lg'>Item Name</h1>
-                                    <p className='text-gray-500 text-sm'>Packing Type</p>
+                                    <button className='w-7 h-7 mr-3 rounded-md bg-gray-400 text-white'>+</button>
+                                    <input type="number" name="" id="" className='rounded-md border-none decoration-transparent text-center w-24' />
+                                    <button className='w-7 h-7 ml-3 rounded-md bg-gray-400 text-white'>-</button>
                                 </div>
-                                <div className='pt-5'>
-                                    <p>
-                                        ✅ in stock
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div>
-                            <div className='text-center flex flex-col justify-center items-center'>
-                                <input type="number" name="" id="" className='rounded-md border-none decoration-transparent'/>
-                                <a className=' pt-1 text-blue-600'>remove</a>
-                            </div>
-                        </div>
-                        <div>
-                            <h1 className='text-xl'>
-                                $35.00
-                            </h1>
-                        </div>
-                    </div>
-                    <div className='flex justify-between pt-8'> 
-                        <div className='flex justify-center items-center gap-5'>
-                            <div className='h-28 w-28'>
-                                <img src={driedFruit4} alt="" className='w-full h-full object-contain rounded-lg' />
-                            </div>
-                            <div>
-                                <div>
-                                    <h1 className='text-lg'>Item Name</h1>
-                                    <p className='text-gray-500 text-sm'>Packing Type</p>
-                                </div>
-                                <div className='pt-5'>
-                                    <p>
-                                        ✅ in stock
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div>
-                            <div className='text-center flex flex-col justify-center items-center'>
-                                <input type="number" name="" id="" className='rounded-md border-none decoration-transparent'/>
                                 <a className=' pt-1 text-blue-600'>remove</a>
                             </div>
                         </div>
@@ -132,7 +150,7 @@ const Cart = () => {
                     </div>
                 </div>
             </div>
-            <div className='bg-gray-200 mt-10 p-4 flex flex-cols'>
+            <div className='bg-gray-200 mt-10 p-4 flex flex-cols rounded-lg'>
                 
                 Subtotal : 350<br/>
                 Shipping: 150<br/>
