6a0b82dd11f6b117fcd9f7bc87f758e3a29b763c
Shinzo27 <20bmiit031@gmail.com>
2024-06-12 19:35:46 +0530

    practice

---
+++ Ecomm/src/Components/CartItem/CartItem.jsx
@@ -0,0 +1,105 @@
+import React from "react";
+import driedFruit1 from "../../assets/driedfruit1.jpg";
+
+const CartItem = () => {
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-2 min-[550px]:gap-6 border-t border-gray-200 py-6">
+      <div className="flex items-center flex-col min-[550px]:flex-row gap-3 min-[550px]:gap-6 w-full max-xl:justify-center max-xl:max-w-xl max-xl:mx-auto">
+        <div className="img-box">
+          <img
+            src={driedFruit1}
+            alt="perfume bottle image"
+            className="xl:w-[140px]"
+          />
+        </div>
+        <div className="pro-data w-full max-w-sm ">
+          <h5 className="font-semibold text-xl leading-8 text-black max-[550px]:text-center">
+            Latest N-5 Perfuam
+          </h5>
+          <p className="font-normal text-lg leading-8 text-gray-500 my-2 min-[550px]:my-3 max-[550px]:text-center">
+            Perfumes
+          </p>
+          <h6 className="font-medium text-lg leading-8 text-indigo-600  max-[550px]:text-center">
+            ₹120.00
+          </h6>
+        </div>
+      </div>
+      <div className="flex items-center flex-col min-[550px]:flex-row w-full max-xl:max-w-xl max-xl:mx-auto gap-2">
+        <div className="flex items-center w-full mx-auto justify-center">
+          <button className="group rounded-l-full px-6 py-[18px] border border-gray-200 flex items-center justify-center shadow-sm shadow-transparent transition-all duration-500 hover:shadow-gray-200 hover:border-gray-300 hover:bg-gray-50">
+            <svg
+              className="stroke-gray-900 transition-all duration-500 group-hover:stroke-black"
+              xmlns="http://www.w3.org/2000/svg"
+              width="22"
+              height="22"
+              viewBox="0 0 22 22"
+              fill="none"
+            >
+              <path
+                d="M16.5 11H5.5"
+                stroke=""
+                strokeWidth="1.6"
+                strokeLinecap="round"
+              />
+              <path
+                d="M16.5 11H5.5"
+                stroke=""
+                strokeOpacity="0.2"
+                strokeWidth="1.6"
+                strokeLinecap="round"
+              />
+              <path
+                d="M16.5 11H5.5"
+                stroke=""
+                strokeOpacity="0.2"
+                strokeWidth="1.6"
+                strokeLinecap="round"
+              />
+            </svg>
+          </button>
+          <input
+            type="text"
+            className="border-y border-gray-200 outline-none text-gray-900 font-semibold text-lg w-full max-w-[118px] min-w-[80px] placeholder:text-gray-900 py-[15px] text-center bg-transparent"
+            placeholder="1"
+          />
+          <button className="group rounded-r-full px-6 py-[18px] border border-gray-200 flex items-center justify-center shadow-sm shadow-transparent transition-all duration-500 hover:shadow-gray-200 hover:border-gray-300 hover:bg-gray-50">
+            <svg
+              className="stroke-gray-900 transition-all duration-500 group-hover:stroke-black"
+              xmlns="http://www.w3.org/2000/svg"
+              width="22"
+              height="22"
+              viewBox="0 0 22 22"
+              fill="none"
+            >
+              <path
+                d="M11 5.5V16.5M16.5 11H5.5"
+                stroke=""
+                strokeWidth="1.6"
+                strokeLinecap="round"
+              />
+              <path
+                d="M11 5.5V16.5M16.5 11H5.5"
+                stroke=""
+                strokeOpacity="0.2"
+                strokeWidth="1.6"
+                strokeLinecap="round"
+              />
+              <path
+                d="M11 5.5V16.5M16.5 11H5.5"
+                stroke=""
+                strokeOpacity="0.2"
+                strokeWidth="1.6"
+                strokeLinecap="round"
+              />
+            </svg>
+          </button>
+        </div>
+        <h6 className="text-indigo-600 font-manrope font-bold text-2xl leading-9 w-full max-w-[176px] text-center">
+          ₹360.00
+        </h6>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
--- Ecomm/src/Pages/Cart.jsx
+++ Ecomm/src/Pages/Cart.jsx
@@ -1,169 +1,107 @@
-import React from 'react'
-import Navbar from '../Components/Navbar/Navbar'
-import Footer from '../Components/Footer/Footer'
-import driedFruit4 from '../assets/driedfruit4.jpg'
-import driedFruit2 from '../assets/driedfruit2.jpg'
-import driedFruit1 from '../assets/driedfruit1.jpg'
-import Button from '../Components/Shared/Button'
+import React, { useState } from "react";
+import Navbar from "../Components/Navbar/Navbar";
+import Footer from "../Components/Footer/Footer";
+import driedFruit4 from "../assets/driedfruit4.jpg";
+import driedFruit2 from "../assets/driedfruit2.jpg";
+import driedFruit1 from "../assets/driedfruit1.jpg";
+import Button from "../Components/Shared/Button";
+import CartItem from "../Components/CartItem/CartItem";
 
 const Cart = () => {
+  const [cartItems, setCartItems] = useState([]);
+
   return (
     <>
-        <Navbar/>
-        <div className='container pt-10'>
-            <div className=' p-9  bg-gray-200 rounded-lg'>
-                <div>
-                    <h1 className=' text-2xl font-semibold'>Shopping Cart</h1>
-                </div>
-                <div className='divide-y divide-dashed pt-8 '>
-                    <div className='flex justify-between pt-8'> 
-                        <div className='flex justify-center items-center gap-5'>
-                            <div className='h-28 w-28'>
-                                <img src={driedFruit4} alt="" className='w-full h-full object-contain rounded-lg' />
-                            </div>
-                            <div>
-                                <div>
-                                    <h1 className='text-lg'>Item Name</h1>
-                                    <p className='text-gray-500 text-sm'>Packing Type</p>
-                                </div>
-                                <div className='pt-5'>
-                                    <p>
-                                        ✅ in stock
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div>
-                            <div className='text-center flex flex-col justify-center items-center'>
-                                <div>
-                                    <button className='w-7 h-7 mr-3 rounded-md bg-gray-400 text-white'>+</button>
-                                    <input type="number" name="" id="" className='rounded-md border-none decoration-transparent text-center w-24' />
-                                    <button className='w-7 h-7 ml-3 rounded-md bg-gray-400 text-white'>-</button>
-                                </div>
-                                <a className=' pt-1 text-blue-600'>remove</a>
-                            </div>
-                        </div>
-                        <div>
-                            <h1 className='text-xl'>
-                                $35.00
-                            </h1>
-                        </div>
-                    </div>
-                    <div className='flex justify-between pt-8'> 
-                        <div className='flex justify-center items-center gap-5'>
-                            <div className='h-28 w-28'>
-                                <img src={driedFruit1} alt="" className='w-full h-full object-contain rounded-lg' />
-                            </div>
-                            <div>
-                                <div>
-                                    <h1 className='text-lg'>Item Name</h1>
-                                    <p className='text-gray-500 text-sm'>Packing Type</p>
-                                </div>
-                                <div className='pt-5'>
-                                    <p>
-                                        ✅ in stock
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div>
-                            <div className='text-center flex flex-col justify-center items-center'>
-                                <div>
-                                    <button className='w-7 h-7 mr-3 rounded-md bg-gray-400 text-white'>+</button>
-                                    <input type="number" name="" id="" className='rounded-md border-none decoration-transparent text-center w-24' />
-                                    <button className='w-7 h-7 ml-3 rounded-md bg-gray-400 text-white'>-</button>
-                                </div>
-                                <a className=' pt-1 text-blue-600'>remove</a>
-                            </div>
-                        </div>
-                        <div>
-                            <h1 className='text-xl'>
-                                $35.00
-                            </h1>
-                        </div>
-                    </div>
-                    <div className='flex justify-between pt-8'> 
-                        <div className='flex justify-center items-center gap-5'>
-                            <div className='h-28 w-28'>
-                                <img src={driedFruit2} alt="" className='w-full h-full object-contain rounded-lg' />
-                            </div>
-                            <div>
-                                <div>
-                                    <h1 className='text-lg'>Item Name</h1>
-                                    <p className='text-gray-500 text-sm'>Packing Type</p>
-                                </div>
-                                <div className='pt-5'>
-                                    <p>
-                                        ✅ in stock
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div>
-                            <div className='text-center flex flex-col justify-center items-center'>
-                                <div>
-                                    <button className='w-7 h-7 mr-3 rounded-md bg-gray-400 text-white'>+</button>
-                                    <input type="number" name="" id="" className='rounded-md border-none decoration-transparent text-center w-24' />
-                                    <button className='w-7 h-7 ml-3 rounded-md bg-gray-400 text-white'>-</button>
-                                </div>
-                                <a className=' pt-1 text-blue-600'>remove</a>
-                            </div>
-                        </div>
-                        <div>
-                            <h1 className='text-xl'>
-                                $35.00
-                            </h1>
-                        </div>
-                    </div>
-                    <div className='flex justify-between pt-8'> 
-                        <div className='flex justify-center items-center gap-5'>
-                            <div className='h-28 w-28'>
-                                <img src={driedFruit4} alt="" className='w-full h-full object-contain rounded-lg' />
-                            </div>
-                            <div>
-                                <div>
-                                    <h1 className='text-lg'>Item Name</h1>
-                                    <p className='text-gray-500 text-sm'>Packing Type</p>
-                                </div>
-                                <div className='pt-5'>
-                                    <p>
-                                        ✅ in stock
-                                    </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div>
-                            <div className='text-center flex flex-col justify-center items-center'>
-                                <div>
-                                    <button className='w-7 h-7 mr-3 rounded-md bg-gray-400 text-white'>+</button>
-                                    <input type="number" name="" id="" className='rounded-md border-none decoration-transparent text-center w-24' />
-                                    <button className='w-7 h-7 ml-3 rounded-md bg-gray-400 text-white'>-</button>
-                                </div>
-                                <a className=' pt-1 text-blue-600'>remove</a>
-                            </div>
-                        </div>
-                        <div>
-                            <h1 className='text-xl'>
-                                $35.00
-                            </h1>
-                        </div>
-                    </div>
-                </div>
+      <Navbar />
+      <h2 className="title font-manrope font-bold text-4xl leading-10 mb-8 text-center text-black pt-10">
+            Shopping Cart
+          </h2>
+      { cartItems ? 
+      <section className="py-24 relative">
+        <div className="w-full max-w-7xl px-4 md:px-5 lg-6 mx-auto">
+          
+          <div className="hidden lg:grid grid-cols-2 py-6">
+            <div className="font-normal text-xl leading-8 text-gray-500">
+              Product
             </div>
-            <div className='bg-gray-200 mt-10 p-4 flex flex-cols rounded-lg'>
-                
-                Subtotal : 350<br/>
-                Shipping: 150<br/>
-                Tax: 50<br/>
-                Order Total: 550
+            <p className="font-normal text-xl leading-8 text-gray-500 flex items-center justify-between">
+              <span className="w-full max-w-[260px] lg:pl-44 text-center">
+                Quantity
+              </span>
+              <span className="w-full max-w-[200px] text-center">Total</span>
+            </p>
+          </div>
+            <CartItem/>
+            <CartItem/>
+          <div className="bg-gray-50 rounded-xl p-6 w-full mb-8 max-lg:max-w-xl max-lg:mx-auto">
+            <div className="flex items-center justify-between w-full mb-6">
+              <p className="font-normal text-xl leading-8 text-gray-400">
+                Sub Total
+              </p>
+              <h6 className="font-semibold text-xl leading-8 text-gray-900">
+                ₹360.00
+              </h6>
             </div>
-            <div className=' mt-10 flex flex-cols'>
-                <Button text={"Checkout"}/>
+            <div className="flex items-center justify-between w-full pb-6 border-b border-gray-200"></div>
+            <div className="flex items-center justify-between w-full py-6">
+              <p className="font-manrope font-medium text-2xl leading-9 text-gray-900">
+                Total
+              </p>
+              <h6 className="font-manrope font-medium text-2xl leading-9 text-indigo-500">
+                ₹360.00
+              </h6>
             </div>
+          </div>
+          <div className="flex items-center flex-col sm:flex-row justify-center gap-3 mt-8">
+            <button className="rounded-full py-4 w-full max-w-[280px]  flex items-center bg-indigo-50 justify-center transition-all duration-500 hover:bg-indigo-100">
+              <span className="px-2 font-semibold text-lg leading-8 text-indigo-600">
+                Continue Shopping
+              </span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="22"
+                height="22"
+                viewBox="0 0 22 22"
+                fill="none"
+              >
+                <path
+                  d="M8.25324 5.49609L13.7535 10.9963L8.25 16.4998"
+                  stroke="#4F46E5"
+                  strokeWidth="1.6"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </button>
+            <button className="rounded-full w-full max-w-[280px] py-4 text-center justify-center items-center bg-indigo-600 font-semibold text-lg text-white flex transition-all duration-500 hover:bg-indigo-700">
+              Checkout Order
+              <svg
+                className="ml-2"
+                xmlns="http://www.w3.org/2000/svg"
+                width="23"
+                height="22"
+                viewBox="0 0 23 22"
+                fill="none"
+              >
+                <path
+                  d="M8.75324 5.49609L14.2535 10.9963L8.75 16.4998"
+                  stroke="white"
+                  strokeWidth="1.6"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </button>
+          </div>
         </div>
-        <Footer/>
+      </section> : <div className=" h-screen pt-10">
+            <h2 className="title font-manrope font-bold text-4xl leading-10 mb-8 text-center text-black">
+              No Items Found In Cart
+            </h2>
+          </div>}
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
